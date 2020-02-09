d2061517a9ec5c8ffbe5c5306c064aae82f9ec8c
weifanhaha <aily34809@gmail.com>
2020-02-09 13:00:28 +0800

    Deploy

---
+++ docs/precache-manifest.dc1083fdc4230ffcf1628c2a06b19d56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb0440f6b2c1f98f6380ddb5f3c282a",
+    "url": "/iamweifan/index.html"
+  },
+  {
+    "revision": "db3420f7af2119ca1221",
+    "url": "/iamweifan/static/css/main.eb12c053.chunk.css"
+  },
+  {
+    "revision": "443e140b3bb315de114a",
+    "url": "/iamweifan/static/js/2.f435d3b0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/iamweifan/static/js/2.f435d3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3420f7af2119ca1221",
+    "url": "/iamweifan/static/js/main.fae43fdf.chunk.js"
+  },
+  {
+    "revision": "dac9318dba3ffcdb50b2",
+    "url": "/iamweifan/static/js/runtime-main.997450cb.js"
+  },
+  {
+    "revision": "cd751b843349d6e04738bc14704ea668",
+    "url": "/iamweifan/static/media/home_img.cd751b84.JPG"
+  },
+  {
+    "revision": "cbe0542dd15d6e82f1b638d8e27f080d",
+    "url": "/iamweifan/static/media/me.cbe0542d.jpg"
+  }
+]);
